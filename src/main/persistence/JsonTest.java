dba2fe7b60ea13c8a6ce4506e7c6ca5547074b04
yxing6 <yxing6@students.cs.ubc.ca>
2021-10-19 13:05:54 -0700

    Added file JsonWriter and JsonTest Followed example from JsonSerializationDemo created by Paul Carter

---
+++ src/main/persistence/JsonTest.java
@@ -0,0 +1,12 @@
+package persistence;
+
+import model.Country;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class JsonTest {
+    protected void checkCountry(String countryName, int travelCost, Country country) {
+        assertEquals(countryName, country.getCountryName());
+        assertEquals(travelCost, country.getCost());
+    }
+}
