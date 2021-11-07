92d6268d4c3e00ced1d54c6b41ae4998deaba1a7
yxing6 <yxing6@students.cs.ubc.ca>
2021-11-07 01:25:22 -0700

    added in a right panel to display country name

--- src/main/ui/gui/MainFrame.java
+++ src/main/ui/gui/MainFrame.java
@@ -1,16 +1,78 @@
 package ui.gui;
 
-import javax.swing.*;
-import java.awt.*;
+import model.Country;
+import model.TravelList;
+import model.exception.NegativeCostException;
 
-public class MainFrame extends JFrame {
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class MainFrame extends JFrame implements ListSelectionListener {
     private static final int WIDTH = 800;
     private static final int HEIGHT = 500;
+    private TravelList travelList;
+    private DefaultListModel bucketList;
+    private DefaultListModel visitedList;
+    private JTextField countryName;
+    private JTextField countryCost;
+    private JButton addToBucketList;
+
+
+
+    public void createTravelList() {
+
+        travelList = new TravelList();
+        try {
+            Country countryA = new Country("Canada", 4000);
+            Country countryB = new Country("China", 5000);
+            Country countryC = new Country("Belgian", 6000);
+            Country countryD = new Country("Belgia", 6000);
+            Country countryE = new Country("Belgi", 6000);
+            Country countryF = new Country("Belg", 6000);
+            Country countryG = new Country("Bel", 6000);
+
+            travelList.addCountryToGo(countryA);
+            travelList.addCountryToGo(countryB);
+            travelList.addCountryVisited(countryC);
+            travelList.addCountryVisited(countryD);
+            travelList.addCountryVisited(countryE);
+            travelList.addCountryVisited(countryF);
+            travelList.addCountryVisited(countryG);
+
+        } catch (NegativeCostException e) {
+            fail("Caught unexpected NegativeCostException while the travel cost is valid");
+        }
+
+        bucketList = new DefaultListModel();
+        List<String> bl = travelList.countriesToGo();
+        for (String s: bl) {
+            bucketList.addElement(s);
+        }
+
+        visitedList = new DefaultListModel();
+        List<String> vl = travelList.countriesVisited();
+        for (String s: vl) {
+            visitedList.addElement(s);
+        }
+
+    }
+
 
     // create a main frame, add and display panels
     public MainFrame(String name) {
 
         super(name);
+
+        createTravelList();
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         addComponentsToPane(this.getContentPane());
 
@@ -22,10 +84,10 @@ public class MainFrame extends JFrame {
 
     public void addComponentsToPane(Container pane) {
 
-        pane.add(leftPanel());
+        pane.setLayout(null);
+        pane.add(leftPanel()).setBounds(0, 0, 500, 500);
+        pane.add(rightPanel()).setBounds(500, 0, 300, 500);
 
-//        pane.add(imagePanel(), BorderLayout.WEST);
-//        pane.add(textPanel2(), BorderLayout.PAGE_END);
 
     }
 
@@ -36,19 +98,42 @@ public class MainFrame extends JFrame {
 
         // set up a world map on the top left of the frame
         JLabel worldLabel = new JLabel();
+//        worldLabel.setHorizontalTextPosition(SwingConstants.CENTER);
+//        worldLabel.setVerticalTextPosition(SwingConstants.CENTER);
         ImageIcon worldMap = createImageIcon();
         worldLabel.setIcon(worldMap);
-        leftPanel.add(worldLabel).setBounds(20, 0, 400, 300);
+        leftPanel.add(worldLabel).setBounds(20, 0, 500, 300);
 
         // set up label and text field on the bottom left of the frame
         JLabel countryName = new JLabel("Country Name:");
         leftPanel.add(countryName).setBounds(20, 310, 120, 25);
-        JTextField countryNameBox = new JTextField();
-        leftPanel.add(countryNameBox).setBounds(150, 310, 120,25);
+        this.countryName = new JTextField();
+        leftPanel.add(this.countryName).setBounds(150, 310, 120,25);
         JLabel countryCost = new JLabel("Country Cost:");
         leftPanel.add(countryCost).setBounds(20, 350, 120, 25);
-        JTextField countryCostBox = new JTextField();
-        leftPanel.add(countryCostBox).setBounds(150, 350, 120,25);
+        this.countryCost = new JTextField();
+        leftPanel.add(this.countryCost).setBounds(150, 350, 120,25);
+
+        this.addToBucketList = new JButton("Add this country to your bucket list");
+        leftPanel.add(addToBucketList).setBounds(200, 400, 150, 25);
+        addToBucketList.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                {
+                    String countryNameString = countryName.getText();
+                    int countryCostInteger = Integer.parseInt(countryCost.getText());
+
+                    try {
+                        Country country = new Country(countryNameString, countryCostInteger);
+                    } catch (NegativeCostException ex) {
+                        ex.printStackTrace();
+                    }
+
+
+                }
+            }
+        });
+
         return leftPanel;
     }
 
@@ -57,7 +142,7 @@ public class MainFrame extends JFrame {
 
         ImageIcon imageIcon = new ImageIcon("./data/world-map-2500.jpg");
         Image bigMap = imageIcon.getImage();
-        Image smallMap = bigMap.getScaledInstance(400, 250, java.awt.Image.SCALE_SMOOTH);
+        Image smallMap = bigMap.getScaledInstance(450, 250, java.awt.Image.SCALE_SMOOTH);
         imageIcon = new ImageIcon(smallMap);
 
         return imageIcon;
@@ -81,7 +166,37 @@ public class MainFrame extends JFrame {
     }
 
 
+    public JPanel rightPanel() {
+        JPanel rightPanel = new JPanel();
+        rightPanel.setLayout(null);
 
+        JLabel buLabel = new JLabel("Your Bucket List");
+        JList buList = new JList(bucketList);
+        buList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+//        buList.setSelectedIndex(0);
+//        buList.addListSelectionListener(this);
+//        buList.setVisibleRowCount(5);
+//        JScrollPane listScrollPane = new JScrollPane(buList);
+
+        JLabel viLabel = new JLabel("Your Visited List");
+        JList viList = new JList(visitedList);
+        viList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+//        viList.setSelectedIndex(0);
+//        viList.addListSelectionListener(this);
+//        viList.setVisibleRowCount(5);
+//        JScrollPane listScrollPane2 = new JScrollPane(viList);
+        rightPanel.add(buLabel).setBounds(0, 0, 100, 30);
+        rightPanel.add(buList).setBounds(0, 40, 100, 150);
+        rightPanel.add(viLabel).setBounds(0,200, 100, 30);
+        rightPanel.add(viList).setBounds(0, 240, 100, 150);
+
+        return rightPanel;
+    }
+
+    @Override
+    public void valueChanged(ListSelectionEvent e) {
+
+    }
 //    public JPanel imagePanel() {
 //        JPanel imagePanel = new JPanel();
 //
