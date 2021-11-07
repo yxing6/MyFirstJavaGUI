c554839827773659f88d5304861af0526ae07483
yxing6 <yxing6@students.cs.ubc.ca>
2021-11-06 21:29:22 -0700

    Added world image to panel

--- src/main/ui/gui/MainFrame.java
+++ src/main/ui/gui/MainFrame.java
@@ -23,24 +23,31 @@ public class MainFrame extends JFrame {
 
     public void addComponentsToPane(final Container pane) {
 
-        pane.add(textPanel());
-        pane.add(textPanel2(),BorderLayout.WEST);
+        pane.add(leftPanel());
+
+//        pane.add(imagePanel(), BorderLayout.WEST);
+//        pane.add(textPanel2(), BorderLayout.PAGE_END);
 
     }
 
-    public JPanel textPanel() {
+    public JPanel leftPanel() {
         JPanel textPanel = new JPanel();
         textPanel.setLayout(null);
-        textPanel.setBackground(new Color(0x929BA7));
+//        textPanel.setBackground(new Color(0x929BA7));
+
+        JLabel worldLabel = new JLabel();
+        ImageIcon worldMap = createImageIcon();
+        worldLabel.setIcon(worldMap);
+        textPanel.add(worldLabel).setBounds(20, 0, 400, 300);
 
         JLabel countryName = new JLabel("Country Name:");
-        textPanel.add(countryName).setBounds(10, 20, 120, 25);
+        textPanel.add(countryName).setBounds(20, 310, 120, 25);
         JTextField countryNameBox = new JTextField();
-        textPanel.add(countryNameBox).setBounds(100, 20, 120,25);
+        textPanel.add(countryNameBox).setBounds(150, 310, 120,25);
         JLabel countryCost = new JLabel("Country Cost:");
-        textPanel.add(countryCost).setBounds(10, 50, 120, 25);
+        textPanel.add(countryCost).setBounds(20, 350, 120, 25);
         JTextField countryCostBox = new JTextField();
-        textPanel.add(countryCostBox).setBounds(100, 50, 120,25);
+        textPanel.add(countryCostBox).setBounds(150, 350, 120,25);
         return textPanel;
     }
 
@@ -60,4 +67,25 @@ public class MainFrame extends JFrame {
         textPanel.add(countryCostBox);
         return textPanel;
     }
+
+    public ImageIcon createImageIcon() {
+
+        ImageIcon imageIcon = new ImageIcon("./data/world-map-2500.jpg");
+        Image bigMap = imageIcon.getImage();
+        Image smallMap = bigMap.getScaledInstance(400, 250, java.awt.Image.SCALE_SMOOTH);
+        imageIcon = new ImageIcon(smallMap);
+
+        return imageIcon;
+    }
+
+//    public JPanel imagePanel() {
+//        JPanel imagePanel = new JPanel();
+//
+//        JLabel worldLabel = new JLabel(" ");
+//        ImageIcon worldMap = createImageIcon();
+//        worldLabel.setIcon(worldMap);
+//
+//        imagePanel.add(worldLabel);
+//        return imagePanel;
+//    }
 }
